0492e24fc13ebc9dec0daaa0f22b759c0a1e7e7e
yonuz0219 <121652974+yonuz0219@users.noreply.github.com>
2025-08-23 14:15:09 +0900

    Add files via upload

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,11 +1,12 @@
 let num = 10;
 
-if(num%3==0){
-    console.log('３の倍数です');
+
+if(num%3==0 && num%5==0){
+    console.log('３と５の倍数です');
 }else if(num%5==0){
     console.log('５の倍数です');
-}else if(num%3==0 && num%5==0){
-    console.log('３と５の倍数です');
+}else if(num%3==0){
+    console.log('３の倍数です');
 }else{
     console.log(num);
 }
