8a5d548509ad38bc994b6785b139356cbe049506
yonuz0219 <121652974+yonuz0219@users.noreply.github.com>
2025-08-26 22:18:00 +0900

    Add files via upload

--- kadai_20/event.js
+++ kadai_20/event.js
@@ -1,13 +1,14 @@
 //id=btnを定数に代入
 const textBtn=document.getElementById('btn');
 let textUnclicked='ボタンをクリックしてください';
-let textClicked='ボタンをクリックしました'
+let textClicked='ボタンをクリックしました';
+
+ //id=textの要素を代入
+ const hText=document.getElementById('text');
+ //const hText=document.querySelector('h2');
+
 //id=btnをクリックしたらイベント開始
 textBtn.addEventListener('click',()=>{
-    //id=textの要素を代入
-    const hText=document.getElementById('text');
-    //const hText=document.querySelector('h2');
-
     if(hText.textContent==textUnclicked){
         hText.textContent=textClicked;
     }else{
